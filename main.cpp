718f3e4c97b5c091fbefaef849619d118654a3bf
Diego <dc_barrientos@yahoo.com.ar>
2020-01-21 16:43:34 -0300

    Creacion del proyecto

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<allegro.h>
+#include<iostream>
+
+using namespace std;
+int main(int argc, char *argv[]){
+    cout << "Hola mundo" << endl;
+    allegro_init();
+
+    return 1;
+}
+
+END_OF_MAIN()
